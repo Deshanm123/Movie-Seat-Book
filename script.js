33ff90bbfc9e6034d350b83142a5df25c6a10b77
Deshanm123 <deshanm123@gmail.com>
2020-06-05 23:43:15 +0530

    ongoing

---
+++ script.js
@@ -0,0 +1,8 @@
+const container= document.querySelector('.container');
+// all the uncoccupied seats on the row
+const seats = document.querySelectorAll(".row.seat:not(.occupied");
+const count=document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie"); 
+const ticketPrice = movieSelect.value;
+
